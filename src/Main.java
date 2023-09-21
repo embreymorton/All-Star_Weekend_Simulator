f70a08dabeec7349dbe253cfc218bcffcdd116a4
embreymorton <111366508+embreymorton@users.noreply.github.com>
2023-09-21 00:56:09 -0400

    Removed Unwanted Files

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
+        
         ThreePointContest tpc = new ThreePointContest(
                 PlayerList.klay_thompson,
-                PlayerList.reggie_miller,
-                PlayerList.michael_jordan,
+                PlayerList.buddy_hield,
+                PlayerList.kevin_durant,
                 PlayerList.lebron_james,
-                PlayerList.larry_bird,
+                PlayerList.glen_rice,
                 PlayerList.steph_curry);
 
         DunkContest dc = new DunkContest(
-                PlayerList.michael_jordan,
+                PlayerList.aaron_gordon,
+                PlayerList.mac_mcclung,
                 PlayerList.vince_carter,
-                PlayerList.lebron_james,
-                PlayerList.kobe_bryant);
+                PlayerList.zach_lavine);
 
         AllStarWeekend asw = new AllStarWeekend(tpc, dc);
         asw.simDetailed();
